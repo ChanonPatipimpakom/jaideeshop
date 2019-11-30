d331ecc7d1e0db61b21c649dec26930070cc26cf
ChanonPatipimpakom <chanon.white.11@gmail.com>
2019-11-30 17:02:35 +0700

    new

--- jaideeshop/savepro.php
+++ jaideeshop/savepro.php
@@ -1,9 +1,15 @@
 <?php
+    echo ini_get("upload_max_filesize")."<br>";
     $allowType=["jpg","jpeg","gif","png","tif","tiff"];
     $fileType=explode("/",$_FILES["filePic"]["type"]);
+    $size = $_FILES["filePic"]["size"]/1024/1024;
+    
     if(!in_array($fileType[1],$allowType)){
         //เมื่ออัพโหลดไฟล์ที่ไม่ตรงกัน
         echo "Non-image file is not allowed";
+    }
+    else if($size>1.00){
+        echo "file size exceeds the maximun treshold.";
     }
     else{
         echo "Name: ".$_FILES["filePic"]["name"] . "<br>";
