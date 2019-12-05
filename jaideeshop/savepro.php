b56ebcab5dbf085f59af6e519b4df71f73c7fd94
ChanonPatipimpakom <chanon.white.11@gmail.com>
2019-12-05 11:01:24 +0700

    last

--- jaideeshop/savepro.php
+++ jaideeshop/savepro.php
@@ -1,5 +1,6 @@
 <?php
-    echo ini_get("upload_max_filesize")."<br>";
+    include("connect.php");
+    //echo ini_get("upload_max_filesize")."<br>";
     $allowType=["jpg","jpeg","gif","png","tif","tiff"];
     $fileType=explode("/",$_FILES["filePic"]["type"]);
     $size = $_FILES["filePic"]["size"]/1024/1024;
@@ -12,11 +13,28 @@
         echo "file size exceeds the maximun treshold.";
     }
     else{
-        echo "Name: ".$_FILES["filePic"]["name"] . "<br>";
+        $name = $_POST['txtName'];
+        $desc = $_POST['txtDescrip'];
+        $price = $_POST['txtPrice'];
+        $unitInStock = $_POST['txtStock'];
+        $filename = $_FILES["filePic"]["name"];
+        /*echo "Name: ".$_FILES["filePic"]["name"] . "<br>";
         echo "Size: ".$_FILES["filePic"]["size"] . "<br>";
         echo "Temp name: ".$_FILES["filePic"]["tmp_name"] . "<br>";
-        echo "Error: ".$_FILES["filePic"]["error"] . "<br>";
+        echo "Error: ".$_FILES["filePic"]["error"] . "<br>";*/
 
         move_uploaded_file($_FILES["filePic"]["tmp_name"],"image/".$_FILES["filePic"]["name"]);
+
+        $sqlInsert = "INSERT INTO product (name,description,price,unitInStock,picture) VALUE('$name', '$desc', '$price', '$unitInStock', '$filename')";
+
+        //echo $sqlInsert;
+        $result = $conn->query($sqlInsert);
+            if($result){
+                echo "<script> alert('Inser Product Complete'); </script>"; 
+                header("Location: index.php");
+            }
+            else{
+                echo "Error during insert product: ".$conn->error;
+            }
     }
 ?>
